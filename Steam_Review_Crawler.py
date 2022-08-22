82ecb1e827206f5330615503ead5ce6396ebeea7
Ishaq <37389192+SSJAfroGuy@users.noreply.github.com>
2022-08-22 17:49:40 +0100

    Added base functionality to grab specific information
    Figured out how to grab the relevant information from the API
    Currently this will take the first review, and store the information that will be required for the final version

--- Steam_Review_Crawler.py
+++ Steam_Review_Crawler.py
@@ -2,7 +2,8 @@ import requests
 import json
 
 # Ask user to input the ID of their game - P5S:1382330
-appid = input("Please enter the steam ID: ")
+# appid = input("Please enter the steam ID: ")
+appid = 1382330
 review_store = "Processed_Reviews.txt"
 open(review_store, 'w').close()
 # review_type = input("Would you like your results sorted by date? (Y/N)")
@@ -19,17 +20,60 @@ open(review_store, 'w').close()
 
 
 # Take the input id and append it to the steam review API
-url = "https://store.steampowered.com/appreviews/" + str(appid) + "?json=1"
+url = f'https://store.steampowered.com/appreviews/{appid}?json=1'
 
 # request GET from the given url
 response = requests.get(url)
 
 # Process the resonse in JSON format and print 
-review = response.json()
-print(review)
+# review = response.json()
+# print(review)
 
-with open(review_store, 'w') as f:
-    json.dump(review, f, indent=4)
+# with open(review_store, 'w') as f:
+    # json.dump(review, f, sort_keys=True, ensure_ascii=False, indent=4)
 
-   # processed_review = json.loads(review())
+# review_str = json.dumps(review, indent=2)
 
+# processed_review = review_str[0]['success']
+# print(processed_review)
+
+data = json.loads(response.text)
+
+#placeholder id generation
+id = str(data['reviews'][0]['author']['steamid']) + str(appid)
+if (data['reviews'][0]['steam_purchase']) == True:
+    review_source = "steam"
+else:
+    review_source = "other"
+
+print(id)
+print(data['reviews'][0]['author']['steamid'])
+print(data['reviews'][0]['timestamp_created'])
+print(data['reviews'][0]['author']['playtime_forever'])
+print(data['reviews'][0]['review'])
+print(data['reviews'][0]['comment_count'])
+print(review_source)
+print(data['reviews'][0]['votes_up'])
+print(data['reviews'][0]['votes_funny'])
+print(data['reviews'][0]['voted_up'])
+
+
+
+
+    
+
+
+# {
+# "id": “your generated id”
+# "author": “e605d04b-1b85-44f7-a693-ce0d87176512”,
+# "date":”2020-01-01”,
+# "hours":100,
+# "content":”Lorem ipsum dolor sit amet”,
+# "comments":4,
+# "source":”steam”,
+# "helpful":6,
+# "funny":3,
+# "recommended": True,
+# "franchise":”A franchise name”,
+# "gameName":”A game Name”
+# }
