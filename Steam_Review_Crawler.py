aeeeaf5660bcf572c74c97112a3b4e18392ab19f
Ishaq <37389192+SSJAfroGuy@users.noreply.github.com>
2022-09-09 14:31:43 +0100

    Refactored code + Fixed bugs
    Changed many of the existing functions to be more customisable when called
    
    Fixed the URL generation - many  parameters can be changed now
    
    Fixed a bug with ID generation
    
    Review cursor variable now has use and allows more than the maximum of 100 reviews to be grabbed - This feature has been tested although it's not currently in this version
    
    Added some more descriptive comments to the code
    
    Changed the method of storing the data to the final list
    
    Current state of this program only needs to properly grab the total number of reviews
    If this number is more than 5000, the total number that will be grabbed will be 5000 (Question to ask may be if more than 5000 want to be grabbed, and stored in a different file, wording in the brief suggests this could be an option)
    If the number is less than 5000, divide it by 100 to get the least number of total full 100 requests. the decimal point number will always be the leftover number to grab.
    e.g 3467/100 = 34.67
    Grab 100 34 times
    0.67 is leftover
    67 reviews are remaining to grab

--- Steam_Review_Crawler.py
+++ Steam_Review_Crawler.py
@@ -2,25 +2,29 @@ import requests
 import json
 import uuid
 import random
+import urllib.parse
 
-# Ask user to input the ID of their game - P5S:1382330
-appid = input("Please enter the steam ID: ")
-franchise = input("Please enter the franchise of your game: ")
-gameName = input("Please enter the name of your game: ")
-# review_type = input("Would you like your results sorted by date? (Y/N)")
-
-# Use https://partner.steamgames.com/doc/webapi/ISteamApps to gather full list of steam apps and use that as a reference to know if the entered ID is valid
-# That would also allow the franchise and name of the game to be passed through - user confirmation
-# Could allow game name entry instead of ID too for ease of use
-# Couldn't I just donwload that once, have it as a text file and have an option to update it if it's out of date, whilst not relying on waitinf for the 100k+ steam apps to be loaded into a text file each time :v
-
+# Set variables for quick testing
 review_cursor = "*"
+review_num = 0
+review_count = 100
+
+# Define random numbers to the ID variables 
 rnduuid=random.Random()
 rndid=random.Random()
-review_num = 0
-# Clear text file upon run - redundant as this happens at the end - but was kept in for the sake of testing
-review_store = "Processed_Reviews.txt"
-open(review_store, 'w').close()
+
+# Get the app id from user input (Currently a static value for tests)
+def __get_app_id__():    
+    appid = 1382330 #input("Please enter the steam ID: ")
+    return appid
+appid = __get_app_id__()
+
+# Generate the name of the output file (static value for now)
+def __name_review_file__():
+    review_store = "Processed_Reviews.txt"
+    return review_store
+
+review_store = __name_review_file__()
 
 # Randomise function for the ID and UUID generation, ensures the same seed is easily replicable so the ID values can be the same with the same input
 def __randomise_id__(id):
@@ -28,68 +32,53 @@ def __randomise_id__(id):
     rnd = uuid.UUID(int=rndid.getrandbits(128), version=4)
     return rnd
 
-# request GET from the given url, passes through user input for appid
-def __generate_url__(cursor):
-    url = f'https://store.steampowered.com/appreviews/{appid}?json=1'
+# Generate the url to the steam API
+def __generate_url__(appid, filter, language, day_range, cursor, review_type, purchase_type, num_per_page):
+    url = f'https://store.steampowered.com/appreviews/{appid}?json=1&filter={filter}&language={language}&day_range={day_range}&cursor={cursor}&review_type={review_type}&purchase_type={purchase_type}&num_per_page={num_per_page}'
     print(url)
     return url
 
 # Return the data in a readable JSON format from the URL generated 
 def __generate_data__():
-    processed_url = __generate_url__(review_cursor)
+    processed_url = __generate_url__(appid, "all", "english", 365, review_cursor, "all", "all", review_count)
     response = requests.get(processed_url)
     data = json.loads(response.text)
     return data
 
-
 processed_data = __generate_data__()
 
-#placeholder id generation - This became un-placeholder, steamid + appid strings added together to generate unique number
-review_id = str(processed_data['reviews'][0]['author']['steamid']) + str(appid)
+# Update cursor to grab the next batch of reviews - Uses URL encoding to ensure characters are URL friendly
+def __generate_cursor__():    
+    review_cursor = urllib.parse.quote(processed_data['cursor'])
+    return review_cursor
 
-# I didn't understand what "source" was supposed to be at first and didn't realise it was passed through by the user? In theory all of these reviews will be "steam" hindsight op
-if (processed_data['reviews'][0]['steam_purchase']) == True:
-    review_source = "steam"
-else:
-    review_source = "other"
-
-# Here for seeing results in terminal
-# print(review_id)
-# print(processed_data['reviews'][0]['author']['steamid'])
-# print(processed_data['reviews'][0]['timestamp_created'])
-# print(processed_data['reviews'][0]['author']['playtime_forever'])
-# print(processed_data['reviews'][0]['review'])
-# print(processed_data['reviews'][0]['comment_count'])
-# print(review_source)
-# print(processed_data['reviews'][0]['votes_up'])
-# print(processed_data['reviews'][0]['votes_funny'])
-# print(processed_data['reviews'][0]['voted_up'])
-# print(franchise)
-# print(gameName)
-# print(processed_data['cursor'])
-
+# Create a list which will store the data from each list. This will be the output
 full_review_list = []
-# This part should be itterated up to 5000 times - Check the total number of reviews first, if > 5000, limit it to 5000. if < 5000, then only go up to the returned total review number
-# Each iteration passes the newly generated cursor value into my generate_url function
 
-
-# Create a list, and iterate through it for the processed data to add each value - This is how the info I want is extracted and the parts that aren't required get left out 
+# Create a list to store the currently processed reviews in
 review_list = []
 
-for review in processed_data:
-    id          = review_id
+# ID generation uses the author ID with the Game ID appended on as the seed
+# Iterate based on total number of reviews grabbed (this might fail with less than 100 reviews total rip lol)
+# Add a check for the literal grabbed number of reviews to fix the above error
+while review_count > review_num:
+    print("Current review num: ", review_num)
+    print("Total review length: ", review_count)    
+    id          = str(processed_data['reviews'][review_num]['author']['steamid']) + str(appid)
     author      = processed_data['reviews'][review_num]['author']['steamid']
     date        = processed_data['reviews'][review_num]['timestamp_created']
     hours       = processed_data['reviews'][review_num]['author']['playtime_forever']
     content     = processed_data['reviews'][review_num]['review']
     comments    = processed_data['reviews'][review_num]['comment_count']
-    source      = review_source
+    source      = processed_data['reviews'][review_num]['steam_purchase']
     helpful     = processed_data['reviews'][review_num]['votes_up']
     funny       = processed_data['reviews'][review_num]['votes_funny']
     recommended = processed_data['reviews'][review_num]['voted_up']
-    uuid_str = str((__randomise_id__(author)))
     id_str   = str((__randomise_id__(id)))
+    uuid_str = str((__randomise_id__(author)))
     review_num += 1
+    # print(review_num)
+    # print(review_cursor)    
 
 # Create an easy to read layout with the values
     review_item = {
@@ -103,14 +92,47 @@ for review in processed_data:
         'helpful': helpful,
         'funny': funny,
         'recommended': recommended,
-        'franchise': franchise,
-        'gameName': gameName
+        # 'franchise': franchise,
+        # 'gameName': gameName
         }
 
-    review_list.append(review_item)
+    # Temp store to add to final output 
+    review_list.append(review_item)    
+    print("Current review length: ", len(review_list))
+        
+# Final output will be from this
+full_review_list.extend(review_list)
+print("Full review length: ", len(full_review_list))
 
-full_review_list.append(review_list)
-
-# Clear and write the review_list to the review file
+# Clear and write the full_review_list to the review file
 with open(review_store, 'w') as f:
-    json.dump(review_list, f, ensure_ascii=True, indent=2)
+    json.dump(full_review_list, f, ensure_ascii=True, indent=2)
+
+
+# EVERYTHING BELOW IS TO TEST IN TERMINAL. 
+# --------------------------------------------
+# print("First batch of reviews")
+# print(review_id)
+# print(processed_data['reviews'][0]['author']['steamid'])
+# print(processed_data['reviews'][0]['timestamp_created'])
+# print(processed_data['reviews'][0]['author']['playtime_forever'])
+# print(processed_data['reviews'][0]['review'])
+# print(processed_data['reviews'][0]['comment_count'])
+# print(processed_data['reviews'][0]['votes_up'])
+# print(processed_data['reviews'][0]['votes_funny'])
+# print(processed_data['reviews'][0]['voted_up'])
+# print(processed_data['cursor'])
+
+# processed_data = __generate_data__()
+
+# print("Second batch of reviews")
+# print(review_id)
+# print(processed_data['reviews'][0]['author']['steamid'])
+# print(processed_data['reviews'][0]['timestamp_created'])
+# print(processed_data['reviews'][0]['author']['playtime_forever'])
+# print(processed_data['reviews'][0]['review'])
+# print(processed_data['reviews'][0]['comment_count'])
+# print(processed_data['reviews'][0]['votes_up'])
+# print(processed_data['reviews'][0]['votes_funny'])
+# print(processed_data['reviews'][0]['voted_up'])
+# print(processed_data['cursor'])
