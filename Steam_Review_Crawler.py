9be5203b06ba5e5b3b0a6ad5d27cd6945df15afe
Ishaq <37389192+SSJAfroGuy@users.noreply.github.com>
2022-09-15 08:44:09 +0100

    Bug fixes
    Noticed there was an issue with the current url for the steam API
    Using the "all" filter meant the reviews would strangely loop up to around 370, before starting over from the start, this resulted in multiple duplicate reviews in the end result
    
    The "recent" filter seems to fix this issue, and also manages to sort all the reviews by date
    
    I haven't 100% confirmed and tested this means they're all perfectly stored in order of date too, but it seems that they're in order from a relatively small sample size that's been used
    
    Also tidied up the code and removed some irrelevant variables and some of the debugging code that's no longer useful or required - VS Code has a really useful python debugger that I've only recently started using and love

--- Steam_Review_Crawler.py
+++ Steam_Review_Crawler.py
@@ -6,10 +6,11 @@ import urllib.parse
 import time
 
 # Set variables for quick testing
+# This will always be the default initial cursor - This could be passed as a default value for URL generation if none is set though? 
 review_cursor = "*"
-# Review count should be definied by the user and done inside a function? This means changing the division number for the total number of 100 iterations as it'll no longer be 100
+# This number is essentially the default unless the total number of reviews is already below 100. 100 is the maximum and lowers the total requests made so it's the most efficient number to use?
 review_count = 100
-review_num = 0
+# Used to count the total number of reviews that have been cralwed through. This allows an upper limit to be reached
 review_iteration = 0
 
 # Define random numbers to the ID variables 
@@ -39,7 +40,7 @@ def __generate_url__(appid, filter, language, day_range, cursor, review_type, pu
     url = f'https://store.steampowered.com/appreviews/{appid}?json=1&filter={filter}&language={language}&day_range={day_range}&cursor={cursor}&review_type={review_type}&purchase_type={purchase_type}&num_per_page={num_per_page}'
     print(url)
     return url
-processed_url = __generate_url__(appid, "all", "english", 365, review_cursor, "all", "all", review_count)
+processed_url = __generate_url__(appid, "recent", "all", 365, review_cursor, "all", "all", review_count)
 
 # Return the data in a readable JSON format from the URL generated 
 def __generate_data__(processed_url):    
@@ -48,27 +49,14 @@ def __generate_data__(processed_url):
     return data
 processed_data = __generate_data__(processed_url)
 
-print("Total number of reviews for the steam ID: ", processed_data['query_summary']['total_reviews'])
-total_reviews = processed_data['query_summary']['total_reviews']
-
-# Check the total number of reviews for the game
-# If this number is > 5000, limit the number to 5000
-# If the number is < 5000. Divide it by 100 (Max number of reviews in a single request) and iterate by the resulting integer.
-# The number after the decimal is equal to the number of reviews reamaining for the final crawl
-
-def __calculate_iterations__():
-    # this code checks the total reviews and prevents more than 5000 responses being retreived 50 * 100 = 5000
-    # if total_reviews >= 5000:
-    #     return 50, 0
-    # else:
-        # This is the number of times the 100 reviews will be grabbed
-        iterations = total_reviews // 100
-        # This is the single number of reviews that needs to be grabbed after the batches of 100
-        leftover_iterations = total_reviews % 100
-        return iterations, leftover_iterations
-
-crawl_number =__calculate_iterations__()
-print("the total number we will crawl by is: ", crawl_number)
+# Find the current total and set the max number of reviews to be 5000 (Could be a seperate variable?)
+def __set_total_reviews__():
+    print("Total number of reviews for the steam ID: ", processed_data['query_summary']['total_reviews'])
+    total_reviews = processed_data['query_summary']['total_reviews']    
+    if total_reviews > 5000:
+        total_reviews = 5000
+    return total_reviews
+total_reviews = __set_total_reviews__()
 
 # Update cursor to grab the next batch of reviews - Uses URL encoding to ensure characters are URL friendly
 def __generate_cursor__(processed_data):    
@@ -84,117 +72,6 @@ full_review_list = []
 # Create a list to store the currently processed reviews in
 review_list = []
 
-# ID generation uses the author ID with the Game ID appended on as the seed
-
-def __crawl_reviews__(full_requests, leftover_requests, processed_data, review_count, review_num, review_cursor):    
-    while full_requests >= 1:
-        print("Full requests is equal to: ", full_requests)
-        processed_url = __generate_url__(appid, "all", "english", 365, review_cursor, "all", "all", review_count)        
-        
-        processed_data = __generate_data__()
-        print("Processing data")
-        review_cursor = __generate_cursor__()
-        print(review_cursor)
-        full_requests -=1
-        
-    if leftover_requests > 0 & full_requests == 0:
-        print("leftover requests is equal to: ", leftover_requests)
-        review_cursor = __generate_cursor__()
-        # Set the review count to the remainder. This will complete the last batch of reviews falling below the 100 per page standard set before
-        review_count = leftover_requests
-        processed_url = __generate_url__(appid, "all", "english", 365, review_cursor, "all", "all", review_count)        
-        # Create the new data based off the new number per page value
-        # processed_data = __generate_data__()
-        
-        id          = str(processed_data['reviews'][review_num]['author']['steamid']) + str(appid)
-        author      = processed_data['reviews'][review_num]['author']['steamid']
-        date        = processed_data['reviews'][review_num]['timestamp_created']
-        hours       = processed_data['reviews'][review_num]['author']['playtime_forever']
-        content     = processed_data['reviews'][review_num]['review']
-        comments    = processed_data['reviews'][review_num]['comment_count']
-        source      = processed_data['reviews'][review_num]['steam_purchase']
-        helpful     = processed_data['reviews'][review_num]['votes_up']
-        funny       = processed_data['reviews'][review_num]['votes_funny']
-        recommended = processed_data['reviews'][review_num]['voted_up']
-        id_str   = str((__randomise_id__(id)))
-        uuid_str = str((__randomise_id__(author)))
-        review_num += 1            
-    # Create an easy to read layout with the values
-        review_item = {
-            'id': id_str,
-            'author-UUID': uuid_str,
-            'date': date,
-            'hours': hours,
-            'content': content,
-            'comments': comments,
-            'source': source,
-            'helpful': helpful,
-            'funny': funny,
-            'recommended': recommended,
-            # 'franchise': franchise,
-            # 'gameName': gameName
-            }
-
-        # Temp store to add to final output 
-        review_list.append(review_item)    
-        print("Current review length: ", len(review_list))
-        
-        # Final output will be from this
-        full_review_list.extend(review_list)
-        print("Full review length: ", len(full_review_list))
-    return
-# __crawl_reviews__(*crawl_number, processed_data, review_count, review_num, review_cursor)        
-
-def __crawl_test__(full_requests, leftover_requests, processed_data, review_count, review_num, review_cursor):    
-    while full_requests >= 1:
-        for x in processed_data['reviews']:
-            print("Current review num: ", review_num)      
-            print("Total review length: ", review_count)    
-            id          = str(processed_data['reviews'][review_num]['author']['steamid']) + str(appid)
-            author      = processed_data['reviews'][review_num]['author']['steamid']
-            date        = processed_data['reviews'][review_num]['timestamp_created']
-            hours       = processed_data['reviews'][review_num]['author']['playtime_forever']
-            content     = processed_data['reviews'][review_num]['review']
-            comments    = processed_data['reviews'][review_num]['comment_count']
-            source      = processed_data['reviews'][review_num]['steam_purchase']
-            helpful     = processed_data['reviews'][review_num]['votes_up']
-            funny       = processed_data['reviews'][review_num]['votes_funny']
-            recommended = processed_data['reviews'][review_num]['voted_up']
-            id_str   = str((__randomise_id__(id)))
-            uuid_str = str((__randomise_id__(author)))
-            review_num += 1            
-        # Create an easy to read layout with the values
-            review_item = {
-                'id': id_str,
-                'author-UUID': uuid_str,
-                'date': date,
-                'hours': hours,
-                'content': content,
-                'comments': comments,
-                'source': source,
-                'helpful': helpful,
-                'funny': funny,
-                'recommended': recommended,
-                'review number': review_num
-                # 'franchise': franchise,
-                # 'gameName': gameName
-                }
-
-            # Temp store to add to final output 
-            review_list.append(review_item)    
-            print("Current review length: ", len(review_list))    
-    # Final output will be from this
-    full_review_list.extend(review_list)
-    print("Full review length: ", len(full_review_list))
-    full_requests -=1
-    processed_url = __generate_url__(appid, "all", "english", 365, review_cursor, "all", "all", review_count)        
-    processed_data =  __generate_data__(processed_url)
-    review_curosr = __generate_cursor__(processed_data)        
-# __crawl_test__(*crawl_number, processed_data, review_count, review_num, review_cursor) 
-
-# Set the number of full (100) review iterations
-full_iterations = crawl_number[0]
-
 number_reviews_grabbed = processed_data['reviews']
 print("There are: ", len(number_reviews_grabbed), " in this batch")
 
@@ -207,14 +84,12 @@ print("There are: ", len(number_reviews_grabbed), " in this batch")
 #     Add the number of reviews in the batch to the review iteration
 #     check review iteration against total reviews and generate URL - data - cursor accordingly
 
-# Set the max number of reviews to be 5000 (Could be a seperate variable?)
-if total_reviews > 5000:
-    total_reviews = 5000
-
 while review_iteration != total_reviews:
-    
+    # initialise the currnet count for the number of reviews in the batch from the URL 
+    review_num = 0
     for x in processed_data['reviews']:
-        print("Current review num: ", review_num)      
+        print("Current review num: ", review_num)
+        print("Total review num: ", review_iteration)
         # print("Total review length: ", review_count)    
         id          = str(processed_data['reviews'][review_num]['author']['steamid']) + str(appid)
         author      = processed_data['reviews'][review_num]['author']['steamid']
@@ -240,7 +115,8 @@ while review_iteration != total_reviews:
             'helpful': helpful,
             'funny': funny,
             'recommended': recommended,
-            'review number': review_num
+            'review number': review_num,
+            'cursor': review_cursor
             # 'franchise': franchise,
             # 'gameName': gameName
             }
@@ -253,7 +129,7 @@ while review_iteration != total_reviews:
     review_iteration += len(processed_data['reviews'])
     print("The number of reviews is equal to: ", review_iteration)     
     # Initialise the review num count to go through a new list 
-    review_num = 0
+    # review_num = 0
     # Wait 5 seconds - this made no difference for the reviews grabbed, keeping as a safety feature possibly? tbf I am not patient enough to test this :D Nor smart enough to try catch or whatever you'd do instead
     # time.sleep(5)
     
@@ -263,62 +139,13 @@ while review_iteration != total_reviews:
         
         
     # Generate the new information using updated numbers 
-    processed_url = __generate_url__(appid, "all", "english", 365, review_cursor, "all", "all", review_count)        
+    processed_url = __generate_url__(appid, "recent", "all", 365, review_cursor, "all", "all", review_count)
+    print("url has been printed")
     processed_data =  __generate_data__(processed_url)
+    print("data has been processed by the new URL")
     print("RCursor before = ",review_cursor)
     review_cursor = __generate_cursor__(processed_data)
-    print("RCursor after = ",review_cursor)          
-        
-
-
-# while total_reviews >= 1:
-    
-#     for x in processed_data['reviews']:
-#         print("Current review num: ", review_num)      
-#         print("Total review length: ", review_count)    
-#         id          = str(processed_data['reviews'][review_num]['author']['steamid']) + str(appid)
-#         author      = processed_data['reviews'][review_num]['author']['steamid']
-#         date        = processed_data['reviews'][review_num]['timestamp_created']
-#         hours       = processed_data['reviews'][review_num]['author']['playtime_forever']
-#         content     = processed_data['reviews'][review_num]['review']
-#         comments    = processed_data['reviews'][review_num]['comment_count']
-#         source      = processed_data['reviews'][review_num]['steam_purchase']
-#         helpful     = processed_data['reviews'][review_num]['votes_up']
-#         funny       = processed_data['reviews'][review_num]['votes_funny']
-#         recommended = processed_data['reviews'][review_num]['voted_up']
-#         id_str   = str((__randomise_id__(id)))
-#         uuid_str = str((__randomise_id__(author)))       
-#     # Create an easy to read layout with the values
-#         review_item = {
-#             'id': id_str,
-#             'author-UUID': uuid_str,
-#             'date': date,
-#             'hours': hours,
-#             'content': content,
-#             'comments': comments,
-#             'source': source,
-#             'helpful': helpful,
-#             'funny': funny,
-#             'recommended': recommended,
-#             'review number': review_num
-#             # 'franchise': franchise,
-#             # 'gameName': gameName
-#             }
-#         review_num += 1     
-#         # Temp store to add to final output 
-#         review_list.append(review_item)    
-#         print("Current review length: ", len(review_list))
-    
-#     review_iteration -= number_reviews_grabbed
-#     full_iterations -=1        
-#     review_num = 0
-#     time.sleep(5)
-#     processed_url = __generate_url__(appid, "all", "english", 365, review_cursor, "all", "all", review_count)        
-#     processed_data =  __generate_data__(processed_url)
-#     print("RCursor before = ",review_cursor)
-#     review_cursor = __generate_cursor__(processed_data)
-#     print("RCursor after = ",review_cursor)      
-
+    print("RCursor after = ",review_cursor)                  
           
 # Final output will be from this
 full_review_list.extend(review_list)
@@ -327,32 +154,3 @@ print("Full review length: ", len(full_review_list))
 # Clear and write the full_review_list to the review file
 with open(review_store, 'w') as f:
     json.dump(full_review_list, f, ensure_ascii=True, indent=2)
-
-
-# EVERYTHING BELOW IS TO TEST IN TERMINAL. 
-# --------------------------------------------
-# print("First batch of reviews")
-# print(review_id)
-# print(processed_data['reviews'][0]['author']['steamid'])
-# print(processed_data['reviews'][0]['timestamp_created'])
-# print(processed_data['reviews'][0]['author']['playtime_forever'])
-# print(processed_data['reviews'][0]['review'])
-# print(processed_data['reviews'][0]['comment_count'])
-# print(processed_data['reviews'][0]['votes_up'])
-# print(processed_data['reviews'][0]['votes_funny'])
-# print(processed_data['reviews'][0]['voted_up'])
-# print(processed_data['cursor'])
-
-# processed_data = __generate_data__()
-
-# print("Second batch of reviews")
-# print(review_id)
-# print(processed_data['reviews'][0]['author']['steamid'])
-# print(processed_data['reviews'][0]['timestamp_created'])
-# print(processed_data['reviews'][0]['author']['playtime_forever'])
-# print(processed_data['reviews'][0]['review'])
-# print(processed_data['reviews'][0]['comment_count'])
-# print(processed_data['reviews'][0]['votes_up'])
-# print(processed_data['reviews'][0]['votes_funny'])
-# print(processed_data['reviews'][0]['voted_up'])
-# print(processed_data['cursor'])
